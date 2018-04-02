8097f62beec39e2e7d1c1bfa08d249817312e6f1
Nicolas Perrenoud <mail@nicu.ch>
2018-04-02 21:06:24 +0200

    added js for file upload

--- resources/views/volunteering/volunteers/show.blade.php
+++ resources/views/volunteering/volunteers/show.blade.php
@@ -166,14 +166,3 @@
     </ul>
 
 @endsection
-
-@section('script')
-
-        $('input[type="file"].custom-file-input').on('change',function(){
-            //get the file name
-            var fileName = $(this).val();
-            //replace the "Choose a file" label
-            $(this).next('.custom-file-label').html(fileName.replace(/^.*[\\\/]/, ''));
-        })
-
-@endsection
