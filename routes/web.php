803e1ef24a434b9c20aa2cff5b81da119e1bff4a
Nicolas Perrenoud <mail@nicu.ch>
2018-04-03 00:24:28 +0200

    comments

--- routes/web.php
+++ routes/web.php
@@ -207,13 +207,13 @@ Route::group(['middleware' => 'language'], function () {
     // Volunteers
     Route::group(['middleware' => ['auth']], function () {
 
-        // Manage volunteers
+        // Volunteers
         Route::get('/volunteering/volunteers', 'Volunteering\VolunteersController@index')->name('volunteering.volunteers.index');
         Route::get('/volunteering/volunteers/export', 'Volunteering\VolunteersController@export')->name('volunteering.volunteers.export');
         Route::get('/volunteering/volunteers/{volunteer}', 'Volunteering\VolunteersController@show')->name('volunteering.volunteers.show');
         Route::get('/volunteering/volunteers/{volunteer}/vcard', 'Volunteering\VolunteersController@vcard')->name('volunteering.volunteers.vcard');
 
-        // Documents
+        // Volunteers: Documents
         Route::get('/volunteering/volunteers/{volunteer}/document/{document}', 'Volunteering\DocumentsController@download')->name('volunteering.documents.download');
         Route::post('/volunteering/volunteers/{volunteer}/document', 'Volunteering\DocumentsController@store')->name('volunteering.documents.store');
         Route::delete('/volunteering/volunteers/{volunteer}/document/{document}', 'Volunteering\DocumentsController@destroy')->name('volunteering.documents.destroy');
@@ -224,11 +224,12 @@ Route::group(['middleware' => 'language'], function () {
         // Jobs
         Route::get('/volunteering/jobs', 'Volunteering\JobsController@index')->name('volunteering.jobs.index');
 
-        
+        // Profile        
         Route::get('/volunteering/profile', 'Volunteering\ProfileController@show')->name('volunteering.profile.show');
         Route::get('/volunteering/profile/edit', 'Volunteering\ProfileController@edit')->name('volunteering.profile.edit');
         Route::post('/volunteering/profile/edit', 'Volunteering\ProfileController@update')->name('volunteering.profile.update');
 
+        // Profile: Trips
         Route::get('/volunteering/profile/trip/apply', 'Volunteering\ProfileController@createTrip')->name('volunteering.profile.createTrip');
         Route::post('/volunteering/profile/trip/apply', 'Volunteering\ProfileController@storeTrip')->name('volunteering.profile.storeTrip');
     });
