5231b1a9c5985de746cef1c57778e22b16bb4b2d
Nicolas Perrenoud <mail@nicu.ch>
2019-11-26 13:54:51 +0200

    filter doc

--- Modules/Accounting/Resources/assets/js/components/AccountingTransactionFilter.vue
+++ Modules/Accounting/Resources/assets/js/components/AccountingTransactionFilter.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="mb-3">
+
+        <!-- Control buttons -->
         <div class="text-right">
+
+            <!-- Reset button -->
             <b-button variant="warning" @click="resetFilter" v-if="filterChanged" size="sm">
                 <icon name="eraser"></icon> Reset filter
             </b-button>
+
+            <!-- Collapse button -->
             <b-button v-b-toggle.collapse-1 class="m-1" size="sm" variant="secondary">
                 <template v-if="!visible">
                     <icon name="search"></icon> Filter results
@@ -13,25 +19,31 @@
                 </template>
             </b-button>
         </div>
+
+        <!-- Collapsible area -->
         <b-collapse v-model="visible" id="collapse-1">
             <b-card body-class="pb-2">
                 <b-form>
                     <b-form-row>
+                        <!-- Receipt Number -->
                         <b-col cols="12" sm="4" lg="3">
                             <b-form-group label="Receipt No">
                                 <b-form-input type="number" v-model="filter.receipt_no" size="sm" debounce="500" min="1" @keydown.esc="filter.receipt_no = null"></b-form-input>
                             </b-form-group>
                         </b-col>
+                        <!-- Date start -->
                         <b-col cols="6" sm="4" lg="3">
                             <b-form-group label="Date from">
                                 <b-form-input type="date" v-model="filter.date_start" :max="todayDate" size="sm"></b-form-input>
                             </b-form-group>
                         </b-col>
+                        <!-- Date end -->
                         <b-col cols="6" sm="4" lg="3">
                             <b-form-group label="Date to">
                                 <b-form-input type="date" v-model="filter.date_end" :max="todayDate" size="sm"></b-form-input>
                             </b-form-group>
                         </b-col>
+                        <!-- Type -->
                         <b-col cols="6" lg="auto">
                             <b-form-group label="Type">
                                 <b-form-radio-group
@@ -41,6 +53,7 @@
                                 ></b-form-radio-group>
                             </b-form-group>
                         </b-col>
+                        <!-- "Registered today" and "no receipt" checks -->
                         <b-col cols="6" lg="auto">
                             <b-form-group label="Options">
                                 <b-form-checkbox v-model="filter.today">Registered today</b-form-checkbox>
@@ -49,6 +62,7 @@
                         </b-col>
                     </b-form-row>
                     <b-form-row>
+                        <!-- Category -->
                         <b-col cols="12" sm="6" md="3">
                             <b-form-group label="Category">
                                 <b-form-select v-model="filter.category" :options="categories" size="sm">
@@ -58,6 +72,7 @@
                                 </b-form-select>
                             </b-form-group>
                         </b-col>
+                        <!-- Project -->
                         <b-col cols="12" sm="6" md="3">
                             <b-form-group label="Project">
                                 <b-form-select v-model="filter.project" :options="projects" size="sm">
@@ -67,11 +82,13 @@
                                 </b-form-select>
                             </b-form-group>
                         </b-col>
+                        <!-- Description -->
                         <b-col cols="12" sm="6" md="3">
                             <b-form-group label="Description">
                                 <b-form-input type="text" v-model="filter.description" size="sm" debounce="500" @keydown.esc="filter.description = null"></b-form-input>
                             </b-form-group>
                         </b-col>
+                        <!-- Beneficiary -->
                         <b-col cols="12" sm="6" md="3">
                             <b-form-group label="Beneficiary">
                                 <b-form-input type="text" v-model="filter.beneficiary" size="sm" debounce="500" @keydown.esc="filter.beneficiary = null"></b-form-input>
